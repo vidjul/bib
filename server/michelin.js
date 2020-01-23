75f9b294e371b3ed488ccecb58872e5be69d5b88
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-01-24 00:59:47 +0100

    feat(michelin): add address, price range, cooking style to data
    - cleanText util is implemented

--- server/michelin.js
+++ server/michelin.js
@@ -2,6 +2,67 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 /**
+ * Clean a given text by removing white spaces and line breaks
+ * @param {String} text text to clean
+ */
+const cleanText = text => {
+  return text
+    .trim()
+    .replace(/^s+/gm, '')
+    .replace(/\n/gm, '')
+    .replace(/\s{2,}/gm, '');
+};
+
+/**
+ *
+ * @param {Object|Array} elem elem with fields to be cleaned
+ */
+const cleanElem = elem => {
+  if (Array.isArray(elem)) {
+    return elem.map(value => cleanText(value));
+  } else {
+    const res = {};
+    Object.entries(elem).forEach(entry => {
+      res[entry[0]] = cleanText(entry[1]);
+    });
+    return res;
+  }
+};
+
+/**
+ * Return an address object by parsing string in
+ * Street, City, Zipcode, Country
+ * @param {String} addrString
+ */
+const parseAddr = addrString => {
+  const addrArr = addrString.split(',');
+  return cleanElem({
+    street: addrArr[0],
+    city: addrArr[1],
+    zip: addrArr[2],
+    country: addrArr[3]
+  });
+};
+
+/**
+ * Return a price range object and a cooking type array
+ * by parsing a priceString
+ * @param {String} priceString
+ */
+const parsePriceCookingType = priceString => {
+  const cleanedString = cleanText(priceString);
+  const [range, cookingType] = cleanedString.split('•');
+  const rangeMatch = range.match(/(\d+)/g);
+  return {
+    price: {
+      min: rangeMatch[0],
+      max: rangeMatch[1]
+    },
+    cookingType: cleanText(cookingType)
+  };
+};
+
+/**
  * Parse webpage restaurant
  * @param  {String} data - html response
  * @return {Object} restaurant
@@ -9,9 +70,19 @@ const cheerio = require('cheerio');
 const parse = data => {
   const $ = cheerio.load(data);
   const name = $('.section-main h2.restaurant-details__heading--title').text();
+  const address = parseAddr(
+    $(
+      '.section-main ul.restaurant-details__heading--list > li:nth-child(1)'
+    ).text()
+  );
+  const price = parsePriceCookingType(
+    $(
+      '.section-main ul.restaurant-details__heading--list > li.restaurant-details__heading-price'
+    ).text()
+  );
   const experience = $('#experience-section > ul > li:nth-child(2)').text();
 
-  return {name, experience};
+  return Object.assign({}, cleanElem({ name, experience }), { address }, price);
 };
 
 /**
@@ -21,7 +92,7 @@ const parse = data => {
  */
 module.exports.scrapeRestaurant = async url => {
   const response = await axios(url);
-  const {data, status} = response;
+  const { data, status } = response;
 
   if (status >= 200 && status < 300) {
     return parse(data);
