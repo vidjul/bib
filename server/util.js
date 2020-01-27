c9fe0cdd4453d91d025dfb7e4e9542cece86530e
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-01-27 21:22:54 +0100

    feat(util): move helpers functions to util.js

---
+++ server/util.js
@@ -0,0 +1,44 @@
+/**
+ * REQUEST HELPERS
+ */
+
+/**
+ * Pause code execution for an amount of time
+ * @param {Number} ms Number of ms to wait
+ */
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+/**
+ * Clean a given text by removing white spaces and line breaks
+ * @param {String} text text to clean
+ */
+const cleanText = text => {
+  if (!text) {
+    return;
+  }
+  return text
+    .trim()
+    .replace(/^s+/gm, '')
+    .replace(/\n/gm, '')
+    .replace(/\s{2,}/gm, '');
+};
+
+/**
+ *
+ * @param {Object|Array} elem elem with fields to be cleaned
+ */
+const cleanElem = elem => {
+  if (Array.isArray(elem)) {
+    return elem.map(value => cleanText(value));
+  } else {
+    const res = {};
+    Object.entries(elem).forEach(entry => {
+      res[entry[0]] = cleanText(entry[1]);
+    });
+    return res;
+  }
+};
+
+module.exports = { cleanText, cleanElem, sleep };
